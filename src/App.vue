437b8135db04f88db7c303098fc3aa14b34dafeb
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2022-02-08 14:54:56 +0100

    Lista fejléc és oszlopok hozzáadása

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
 <template>
   <div id="app">
+    <table>
+      <thead>
+        <tr>
+        <th>Személy</th>
+        <th>Magasság</th>
+        <th>Ár</th>
+        <th>Műveletek</th>
+        </tr>
+        </thead>
+        <tbody>
+          <tr v-for="statue in statues" v-bind:key="statue.id">
+            <td>{{ statue.person }}</td>
+            <td>{{ statue.height }}</td>
+            <td>{{ statue.price }}</td>
+            <td>
+              <button>Törlés</button>
+              <button>Szerkesztés</button>
+            </td>
+          </tr>        
+        </tbody>      
+    </table>
+    <button @click="betoltes">Betöltés</button>
     
   </div>
 </template>
@@ -10,8 +32,30 @@
 export default {
   name: 'App',
   components: {
-    
-  }
+  },
+  data() {
+    return {
+     statue: {
+        id: null,
+        person: '',
+        height: null,
+        price: null
+      },
+        statues: []
+    }
+  },
+  methods: {
+    async betoltes() {
+      let Response = await fetch('http://127.0.0.1:8000/api/statues')
+      let adat = await Response.json()
+      this.statues = adat
+
+    },
+    mounted() {
+      this.betoltes()
+    }
+  },
+
 }
 </script>
 
